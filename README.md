143b74558071eeb8d61b09a2bcaa374450dab10e
Roi Rodríguez <roi.rodriguez.mendez@gmail.com>
2023-07-15 20:22:45 +0200

    Initial commit. Scripts to open NYT / elpais hard sudokus on f-puzzles, CTC, or SudokuExchange.

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains userscripts i use mainly with f-puzzles.com. Licenced under MIT Licence, see [LICENCE](LICENCE) file.
---
+++ nyt-hard-open-fpuzzles.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name         NYT hard open
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Open NYT sudoku into f-puzzles, sudokuexchange, or CTC app
+// @author       Roi Rodriguez
+// @match        https://www.nytimes.com/puzzles/sudoku/hard
+// @icon         https://www.nytimes.com/games-assets/v2/metadata/nyt-favicon.ico?v=v2307131500
+// @require      https://f-puzzles.com/Compression.js?v=1.11.3
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+     const doShim = () => {
+         const extractBoard = () => {
+             let board = document.getElementsByClassName("su-board")[0];
+             let sudokuStr = board
+                 ? [...board.getElementsByClassName("su-cell")]
+                 .map((el) =>
+                      el.classList.contains("prefilled") ? el.getAttribute("aria-label") : "0"
+                     )
+                 .join("")
+             : "";
+             return sudokuStr;
+         };
+
+         const createFpuzzlesStr = () => {
+             const size = 9;
+             const puzzle = {
+                 "author": "NYT",
+                 "ruleset": "Normal sudoku rules apply.",
+                 "title": "NYT hard " + moment().format('YYYY-MM-DD'),
+                 "size": size,
+                 "grid": []
+             }
+             const sudokuStr = extractBoard();
+             for(var i = 0; i < size; i++) {
+                 puzzle.grid.push([]);
+                 for (var j = 0; j < size; j++) {
+                     let digit = parseInt(sudokuStr.charAt(i * size + j));
+                     puzzle.grid[i].push({
+                         "value": digit,
+                         "given": digit != 0,
+                         "cornerPencilMarks": [],
+                         "centerPencilMarks": [],
+                         "c": 0,
+                         "highlight": 0
+                     });
+                 }
+             }
+             const sudokuFpuzzlesEncodedJson = compressor.compressToBase64(JSON.stringify(puzzle));
+             return sudokuFpuzzlesEncodedJson;
+         }
+
+         const openInSudokuExchange = GM_registerMenuCommand("Open in sudokuexchange", (e) => {
+             let sudokuStr = extractBoard();
+             window.open("https://sudokuexchange.com/play?s=" + sudokuStr, "_blank");
+         });
+
+         const openInFpuzzles = GM_registerMenuCommand("Open in f-puzzles", (e) => {
+             const sudokuFpuzzlesEncodedJson = createFpuzzlesStr();
+             window.open("https://f-puzzles.com/?load=" + sudokuFpuzzlesEncodedJson, "_blank");
+         });
+
+         const openInCTCApp = GM_registerMenuCommand("Open in CTC app", (e) => {
+             const sudokuFpuzzlesEncodedJson = createFpuzzlesStr();
+             window.open("https://app.crackingthecryptic.com/sudoku/?puzzleid=fpuzzles" + sudokuFpuzzlesEncodedJson, "_blank");
+         });
+     };
+
+    doShim();
+})();
